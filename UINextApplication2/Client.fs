9f6bcbe992f86d6282d0ebc068c0a93950336f7c
Jonas Juselius <jonas.juselius@itpartner.no>
2016-06-29 08:12:00 +0200

    First commit. Ugly bugly, and in need of cleanups.

--- UINextApplication2/Client.fs
+++ UINextApplication2/Client.fs
@@ -5,32 +5,75 @@ open WebSharper.JavaScript
 open WebSharper.JQuery
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Html
 
 [<JavaScript>]
-module Client =    
+module Client =
     type IndexTemplate = Templating.Template<"index.html">
 
-    let People =
-        ListModel.FromSeq [
-            "John"
-            "Paul"
-        ]
+    let (|IsPalindrome|_|) s =
+        let r = s |> Array.ofSeq |> Array.rev |> Array.fold (fun a s -> a + string s) ""
+        if s = r && s.Length > 2
+        then Some r
+        else None
+
+    let (|HasTFP|_|) (s : string) =
+        if s.Contains "TromsoFP"
+        then Some s
+        else None
 
     let Main =
         JQuery.Of("#main").Empty().Ignore
 
-        let newName = Var.Create ""
+        let rvInput = Var.Create ""
+        let rvList = Var.Create List.Empty : Var<list<int * string>>
+
+        let doStuff  (_ : Dom.Element) (_ : Dom.Event) =
+            match rvInput.Value with
+            | IsPalindrome s ->
+                let v = rvList.Value
+                let (n, _) = if v.Length = 0 then (0, "") else v.[v.Length - 1]
+                rvList.Value <- (n+1, s) :: v
+            | HasTFP s ->
+                let v = rvList.Value
+                let (n, _) = v.[v.Length - 1]
+                rvList.Value <- (n+1, s) :: v
+            | _ -> ()
 
         IndexTemplate.Main.Doc(
-            ListContainer = [
-                People.View.DocSeqCached(fun name ->
-                    IndexTemplate.ListItem.Doc(Name = View.Const name)
-                )
-            ],
-            Name = newName,
-            Add = (fun el ev ->
-                People.Add(newName.Value)
-                newName.Value <- ""
-            )
+            Body = [
+                h1 [text "Hello TromsoFP!"]
+                p [
+                    text "Enter text:"
+                    Doc.Input [on.change doStuff] rvInput
+                ]
+                pAttr [
+                    attr.classDyn <| rvInput.View.Map (fun x ->
+                        match x with
+                        | IsPalindrome s -> "alert alert-danger"
+                        | _ -> ""
+                    )
+                    ] [
+                    text "You enter:"
+                    textView <| rvInput.View.Map (fun (x  : string )-> x.ToUpper ())
+                ]
+                div [
+                    rvList.View
+                    |> Doc.BindView (fun lst ->
+                        tableAttr [
+                            attr.``class`` "table table-striped"
+                            ] ([
+                                tr [th [text "#"]; th [ text "special"]]
+                            ]
+                            @ (lst |>
+                                List.map (fun (x, y) ->
+                                    tr [
+                                        td [text <| string x]
+                                        td [text y]
+                                    ] :> Doc
+                                )))
+                        )
+                ]
+            ]
         )
         |> Doc.RunById "main"
